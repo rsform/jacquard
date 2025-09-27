5aad02ae050a9d4dda642dd6650480a6d147b2be
Orual <orual@nonbinary.computer>
2025-09-27 13:26:52 -0400

    update ci and formatting

--- nix/modules/devshell.nix
+++ nix/modules/devshell.nix
@@ -1,23 +1,23 @@
-{inputs, ...}: {
-  perSystem = {
-    config,
-    self',
-    pkgs,
-    lib,
-    ...
-  }: {
-    devShells.default = pkgs.mkShell {
-      name = "jacquard-shell";
-      inputsFrom = [
-        self'.devShells.rust
-        config.pre-commit.devShell # See ./nix/modules/pre-commit.nix
-      ];
-      packages = with pkgs; [
-        just
-        nixd # Nix language server
-        bacon
-        rust-analyzer
-      ];
+{ inputs, ... }: {
+  perSystem =
+    { config
+    , self'
+    , pkgs
+    , lib
+    , ...
+    }: {
+      devShells.default = pkgs.mkShell {
+        name = "jacquard-shell";
+        inputsFrom = [
+          self'.devShells.rust
+          config.pre-commit.devShell # See ./nix/modules/pre-commit.nix
+        ];
+        packages = with pkgs; [
+          just
+          nixd # Nix language server
+          bacon
+          rust-analyzer
+        ];
+      };
     };
-  };
 }
--- nix/modules/rust.nix
+++ nix/modules/rust.nix
@@ -1,64 +1,66 @@
-{inputs, ...}: {
+{ inputs, ... }: {
   imports = [
     inputs.rust-flake.flakeModules.default
     inputs.rust-flake.flakeModules.nixpkgs
     # inputs.process-compose-flake.flakeModule
     # inputs.cargo-doc-live.flakeModule
   ];
-  perSystem = {
-    config,
-    self',
-    pkgs,
-    lib,
-    ...
-  }: let
-    inherit (pkgs.stdenv) isDarwin;
-    inherit (pkgs.darwin) apple_sdk;
+  perSystem =
+    { config
+    , self'
+    , pkgs
+    , lib
+    , ...
+    }:
+    let
+      inherit (pkgs.stdenv) isDarwin;
+      inherit (pkgs.darwin) apple_sdk;
 
-    # Common configuration for all crates
-    globalCrateConfig = {
-      crane.clippy.enable = false;
-    };
-
-    # Common build inputs for all crates
-    commonBuildInputs = lib.optionals isDarwin (
-      with apple_sdk.frameworks; [
-        IOKit
-        Security
-        SystemConfiguration
-      ]
-    );
-  in {
-    rust-project = {
-      # Source filtering to avoid unnecessary rebuilds
-      src = lib.cleanSourceWith {
-        src = inputs.self;
-        filter = config.rust-project.crane-lib.filterCargoSources;
+      # Common configuration for all crates
+      globalCrateConfig = {
+        crane.clippy.enable = false;
       };
-      crates = {
-        "jacquard" = {
-          imports = [globalCrateConfig];
-          autoWire = ["crate" "clippy"];
-          path = ./../../crates/jacquard;
-          crane = {
-            args = {
-              buildInputs = commonBuildInputs;
+
+      # Common build inputs for all crates
+      commonBuildInputs = lib.optionals isDarwin (
+        with apple_sdk.frameworks; [
+          IOKit
+          Security
+          SystemConfiguration
+        ]
+      );
+    in
+    {
+      rust-project = {
+        # Source filtering to avoid unnecessary rebuilds
+        src = lib.cleanSourceWith {
+          src = inputs.self;
+          filter = config.rust-project.crane-lib.filterCargoSources;
+        };
+        crates = {
+          "jacquard" = {
+            imports = [ globalCrateConfig ];
+            autoWire = [ "crate" "clippy" ];
+            path = ./../../crates/jacquard;
+            crane = {
+              args = {
+                buildInputs = commonBuildInputs;
+              };
+            };
+          };
+
+          "jacquard-common" = {
+            imports = [ globalCrateConfig ];
+            autoWire = [ "crate" "clippy" ];
+            path = ./../../crates/jacquard-common;
+            crane = {
+              args = {
+                buildInputs = commonBuildInputs;
+              };
             };
           };
         };
-
-        "jacquard-common" = {
-          imports = [globalCrateConfig];
-          autoWire = ["crate" "clippy"];
-          path = ./../../crates/jacquard-common;
-          crane = {
-            args = {
-              buildInputs = commonBuildInputs;
-            };
-          };
-        };
       };
+      packages.default = self'.packages.jacquard;
     };
-    packages.default = self'.packages.jacquard;
-  };
 }
